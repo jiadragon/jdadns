645adad3fb3ad382ba2def9c12aa66b3a9d7ca72
source_server <source_server@example.com>
2016-05-19 08:27:53 +0000

    modify workspace

--- jdadns.py
+++ jdadns.py
@@ -100,7 +100,7 @@ class Redirector(webapp2.RequestHandler):
       if self.request.query_string != '' :
         self.redirect(urllib.unquote(record.url)+'?'+self.request.query_string) # redirect to the HTTP-IN URL with arugments
       else:
-        self.response.out.write(record)#self.redirect(urllib.unquote(record.url)) # redirect to the HTTP-IN URL
+        self.response.out.write(record) #self.redirect(urllib.unquote(record.url)) # redirect to the HTTP-IN URL
       
 app = webapp2.WSGIApplication( [('/', MainPage),
                                 ('/.*',Redirector)],
