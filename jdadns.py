6d89ff82d4453d3a43a9d74412af4f40881658ac
source_server <source_server@example.com>
2016-05-19 13:07:24 +0000

    modify workspace

--- jdadns.py
+++ jdadns.py
@@ -77,7 +77,7 @@ class Main_DNS(webapp2.RequestHandler):
       param4=self.request.get('pass') # the password for modifying the entry
 
       if param2=="" :
-                logging.info('Could not Update Blank Service')
+        logging.info('Could not Update Blank Service')
                 self.response.out.write('Error2')
                 return
  
