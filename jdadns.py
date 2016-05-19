79cd9f5e19afda715d989a0c0d2e02d90f9035f2
source_server <source_server@example.com>
2016-05-19 12:08:21 +0000

    modify workspace

--- jdadns.py
+++ jdadns.py
@@ -48,7 +48,7 @@ class Main_DNS(webapp2.RequestHandler):
         for result in records: 
           if result.hidden == False or got_admin == True:
             self.response.out.write(result.name+',')
-        logging.info('Service List: Success')
+        #logging.info('Service List: Success')
         self.response.out.write('END')  # Cap the list             
  
   def post(self):
