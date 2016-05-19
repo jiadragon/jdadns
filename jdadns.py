bbcdc06f80500dbde71406bb953e920772f6a6ed
JiaDragon Allen <jiadragon.sl@gmail.com>
2016-05-19 13:37:40 +0000

    .

--- jdadns.py
+++ jdadns.py
@@ -70,7 +70,10 @@ class Main_DNS(webapp2.RequestHandler):
         self.response.set_status(401)
         logging.info('Rejected Service: '+param2+' remove. Not Found')
         self.response.out.write('Rejected')
-        
+
+    elif self.request.get('type')=='update': # get the current URL for a given service
+      self.response.out.write('Update')
+      
     elif self.request.get('type')=='retrieve': # get the current URL for a given service
       param2=self.request.get('name')     # the name the service is known by
       record = Service.get_by_key_name(param2)
