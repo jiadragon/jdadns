33bd120376fc1cbce6b00a6cfb3e394d6395779b
source_server <source_server@example.com>
2016-05-19 13:18:56 +0000

    modify workspace

--- jdadns.py
+++ jdadns.py
@@ -136,6 +136,7 @@ class Main_DNS(webapp2.RequestHandler):
         self.response.set_status(401)
         logging.info('Update Service: '+param2+' failed. Invalid Password.')
         self.response.out.write('Rejected')
+        
     elif self.request.get('type')=='retrieve': # get the current URL for a given service
       param2=self.request.get('name')     # the name the service is known by
       record = Service.get_by_key_name(param2)
