d8a0f2a1a9f2f0ba9d553369cf4ec560f5a1afa4
source_server <source_server@example.com>
2016-05-19 23:04:52 +0000

    modify workspace

--- jdadns.py
+++ jdadns.py
@@ -99,7 +99,7 @@ class Main_DNS(webapp2.RequestHandler):
           self.response.out.write('Added')
       elif record.writepass == "" or record.writepass == param4 or (admin_password != 'null' and admin_password == self.request.get('admin')): 
         # record already exists, update it                    
-                newwpass = record.writepass
+        newwpass = record.writepass
                 newrpass = record.readpass
                 newhidden = record.hidden
           
