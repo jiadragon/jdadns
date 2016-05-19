337aac82fae7cc736d7e0f76205228da81ca09a6
source_server <source_server@example.com>
2016-05-19 13:12:21 +0000

    modify workspace

--- jdadns.py
+++ jdadns.py
@@ -111,7 +111,7 @@ class Main_DNS(webapp2.RequestHandler):
             if self.request.get('wpass') == "null":
               newwpass = ""
             else:
-                        newwpass = self.request.get('wpass')
+              newwpass = self.request.get('wpass')
                 if self.request.get('rpass') != "":
                     if self.request.get('rpass') == "null":
                         newrpass = ""
