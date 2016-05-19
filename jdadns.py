baaf6a4c00e38ffdbe18f0cb0c2bc8c3f9dc75d5
source_server <source_server@example.com>
2016-05-19 11:11:07 +0000

    modify workspace

--- jdadns.py
+++ jdadns.py
@@ -128,8 +128,8 @@ class Main_DNS(webapp2.RequestHandler):
             elif self.request.get('hidden') == "0":
               newhidden = False
  
-            # add record, either replacing the deleted one, or adding a new one if it never existed
-            record.url=param3
+          # add record, either replacing the deleted one, or adding a new one if it never existed
+          record.url=param3
                 record.writepass=newwpass
                 record.readpass=newrpass
                 record.hidden=newhidden
