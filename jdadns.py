9df9506e81cffec43cf5fe9da70843ecbdd2036f
source_server <source_server@example.com>
2016-05-19 23:03:01 +0000

    modify workspace

--- jdadns.py
+++ jdadns.py
@@ -91,9 +91,9 @@ class Main_DNS(webapp2.RequestHandler):
             newhidden = True
           newrec=Service(key_name=param2,name=param2,url=param3,writepass=param4,readpass=param4, hidden=newhidden)
           if self.request.get('wpass') != "":
-              newrec.writepass=self.request.get('wpass')
-            if self.request.get('rpass') != "":
-              newrec.readpass=self.request.get('rpass')
+            newrec.writepass=self.request.get('wpass')
+          if self.request.get('rpass') != "":
+            newrec.readpass=self.request.get('rpass')
             newrec.put()
             logging.info('Added Service: '+param2+' (update add)')
             self.response.out.write('Added')
