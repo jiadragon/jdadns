76f0e71382bbc68283df72e94c2c92b8585384af
source_server <source_server@example.com>
2016-05-19 23:00:19 +0000

    modify workspace

--- jdadns.py
+++ jdadns.py
@@ -85,6 +85,7 @@ class Main_DNS(webapp2.RequestHandler):
         if param3 == '':
           logging.info('Updating Service: '+param2+' failed. Blank URL')
           self.response.out.write('Error3')
+          
       self.response.out.write('Update')
       
     elif self.request.get('type') == 'retrieve': # get the current URL for a given service
