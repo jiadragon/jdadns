f787d305d592baef4f3bc14e1c8f67dae5eaeed8
source_server <source_server@example.com>
2016-05-19 11:12:43 +0000

    modify workspace

--- jdadns.py
+++ jdadns.py
@@ -145,7 +145,7 @@ class Main_DNS(webapp2.RequestHandler):
       elif self.request.get('type')=='retrieve': # get the current URL for a given service
         param2=self.request.get('name')     # the name the service is known by
  
-            record = Service.get_by_key_name(param2)
+        record = Service.get_by_key_name(param2)
  
             if record is None:
                 self.response.set_status(200)
