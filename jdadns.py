e61da40f8a449280d478bbfbde428e6073a084ab
JiaDragon Allen <jiadragon.sl@gmail.com>
2016-05-19 22:54:39 +0000

    .

--- jdadns.py
+++ jdadns.py
@@ -56,8 +56,8 @@ class Main_DNS(webapp2.RequestHandler):
         logging.info('Rejected Service: '+param2+' Invalid admin password')
         self.response.out.write('Rejected')
  
-    elif self.request.get('type')=='remove': #removing a service
-      param2=self.request.get('name')     # the name the service is known by
+    elif self.request.get('type') == 'remove': #removing a service
+      param2 = self.request.get('name')     # the name the service is known by
       record = Service.get_by_key_name(param2)
       if record is None:
         self.response.set_status(200)
@@ -72,10 +72,10 @@ class Main_DNS(webapp2.RequestHandler):
         self.response.out.write('Rejected')
 
     elif self.request.get('type')=='update': #removing a service
-      param2=self.request.get('name')     # the name the service is known by
-      param3=self.request.get('url')     # the URL for the web service
-      param4=self.request.get('pass')     # the password for modifying the entry
-      if param2=="":
+      param2 = self.request.get('name')     # the name the service is known by
+      param3 = self.request.get('url')     # the URL for the web service
+      param4 = self.request.get('pass')     # the password for modifying the entry
+      if param2 == "":
         logging.info('Could not Update Blank Service')
         self.response.out.write('Error2')
         return
