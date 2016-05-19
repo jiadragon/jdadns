27d0ebe3873ee805602a9bcd8b6be15d4e8b697d
source_server <source_server@example.com>
2016-05-19 23:02:03 +0000

    modify workspace

--- jdadns.py
+++ jdadns.py
@@ -88,8 +88,8 @@ class Main_DNS(webapp2.RequestHandler):
         else:
           newhidden = False
           if self.request.get('hidden') == '1':
-              newhidden = True
-                    newrec=Service(key_name=param2,name=param2,url=param3,writepass=param4,readpass=param4, hidden=newhidden)
+            newhidden = True
+          newrec=Service(key_name=param2,name=param2,url=param3,writepass=param4,readpass=param4, hidden=newhidden)
                     if self.request.get('wpass') != "":
                         newrec.writepass=self.request.get('wpass')
                     if self.request.get('rpass') != "":
