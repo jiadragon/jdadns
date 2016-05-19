19ee39ccc683fa5f3ee76cbab08028745bac48b6
source_server <source_server@example.com>
2016-05-19 08:45:46 +0000

    modify workspace

--- jdadns.py
+++ jdadns.py
@@ -100,7 +100,7 @@ class Redirector(webapp2.RequestHandler):
       if self.request.query_string != '' :
         return self.redirect(urllib.unquote(record.url)+'?'+self.request.query_string) # redirect to the HTTP-IN URL with arugments
       else:
-        self.response.out.write() # Service wasn't found return self.redirect(urllib.unquote(record.url)) # redirect to the HTTP-IN URL
+        self.response.out.write(urllib.unquote(record.url)) # Service wasn't found return self.redirect(urllib.unquote(record.url)) # redirect to the HTTP-IN URL
       
 app = webapp2.WSGIApplication( [('/', MainPage),
                                 ('/.*',Redirector)],
