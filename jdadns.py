111a0cb58afa46b09746604af6de33b6f509d196
source_server <source_server@example.com>
2016-05-19 22:59:44 +0000

    modify workspace

--- jdadns.py
+++ jdadns.py
@@ -79,8 +79,8 @@ class Main_DNS(webapp2.RequestHandler):
         logging.info('Could not Update Blank Service')
         self.response.out.write('Error2')
         return
+      
       record = Service.get_by_key_name(param2)
- 
             if record is None:
                 if param3 == "":
                     logging.info('Updating Service: '+param2+' failed. Blank URL')
