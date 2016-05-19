41954b6d8dee89091d86bb66ef5362749c418873
JiaDragon Allen <jiadragon.sl@gmail.com>
2016-05-19 11:47:55 +0000

    .

--- jdadns.py
+++ jdadns.py
@@ -15,15 +15,7 @@ class Main_DNS(webapp2.RequestHandler):
   def get(self):
  
     self.response.out.write('None') # Service wasn't found
-    obj_name=self.request.headers.get('X-SecondLife-Object-Name')
-    region=self.request.headers.get('X-SecondLife-Region')
-    local_pos=self.request.headers.get('X-SecondLife-Local-Position')
-  	owner_name=self.request.headers.get('X-SecondLife-Owner-Name')
-    owner_key=self.request.headers.get('X-SecondLife-Owner-Key')
- 
-    admin_password = "adminpassword"        # This password enables you to have unrestricted access to the DNS
-    # Putting "null" in password disables this feature.
- 
+
   def post(self):
    self.get()
  
