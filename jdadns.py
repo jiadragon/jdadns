51dcc754dbd69c8693726e7a443fc5677a88c298
source_server <source_server@example.com>
2016-05-19 12:05:59 +0000

    modify workspace

--- jdadns.py
+++ jdadns.py
@@ -36,8 +36,7 @@ class Main_DNS(webapp2.RequestHandler):
       if admin_password != 'null' and admin_password == self.request.get('admin'):
         newrec = Service.get_by_key_name(param2)
     elif self.request.get('type')=='list': # List the existing services
-        records = Service.all()
- 
+      records = Service.all()
         if records is None:
           logging.info('Service List: Empty')
           self.response.out.write('Empty') # Services weren't found
