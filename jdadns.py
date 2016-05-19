b8d3c406c11d314789031b8d2e43c094d8904bde
source_server <source_server@example.com>
2016-05-19 12:21:23 +0000

    modify workspace

--- jdadns.py
+++ jdadns.py
@@ -53,7 +53,7 @@ class Main_DNS(webapp2.RequestHandler):
                     self.response.out.write('Found')  # service already exists so announce that and do nothing
       else:
         self.response.set_status(401)
-                logging.info('Rejected Service: '+param2+' Invalid admin password')
+              logging.info('Rejected Service: '+param2+' Invalid admin password')
                 self.response.out.write('Rejected')
  
       
