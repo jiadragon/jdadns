b9c2bf88f0803903841bd24659d1d034955e1e51
source_server <source_server@example.com>
2016-05-20 01:06:13 +0000

    modify workspace

--- jdadns.py
+++ jdadns.py
@@ -128,7 +128,8 @@ class Main_DNS(webapp2.RequestHandler):
         record.put()
         logging.info('Updated Service: '+param2)
         self.response.out.write('Updated')
-    	e:
+    	else:
+        
         #self.response.set_status(401)
         #logging.info('Update Service: '+param2+' failed. Invalid Password.')
         #self.response.out.write('Rejected')
